9688b8906678aec34d8f0936c4a4153750f66f0f
Ian293382 <moshou5571228@gmail.com>
2022-05-26 12:50:22 +0800

    install boostrap 5 on rails 6

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -11,3 +11,10 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
+// inside app/frontend/packs/application.js
+
+// Add this line
+import '../js/bootstrap_js_files.js'
