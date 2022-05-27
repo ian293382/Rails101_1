f4189cff65fd5d6e99399c93d4b5600fa0b2c527
Ian293382 <moshou5571228@gmail.com>
2022-05-28 01:02:39 +0800

    move posts to author folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :authors
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: "home#index"
    resources :groups
    resources :homes
+
+   devise_for :authors
    get 'home/index'
+
+  scope module: 'authors' do
+     resources :posts
+  end
 end
