79944635a9139fc35984e14f38932a6733f77f51
Ian293382 <moshou5571228@gmail.com>
2022-05-29 01:17:58 +0800

    add  elements to posts & add posts_edit too create paragraph

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: "home#index"
    resources :groups
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
    get 'home/index'
 
   scope module: 'authors' do
-     resources :posts
+     resources :posts do
+       resources :elements
+     end
   end
 end
